47898778ae820d590301d28864c8cd6471fecdf2
Andrii <andrii.sonsiadlo@gmail.com>
2023-05-08 16:54:06 +0200

    implement A* algorithm

--- Graph.py
+++ Graph.py
@@ -85,98 +85,37 @@ class Graph:
 
         return float('infinity'), None
 
+    def a_star(self, origin, destination):
+        """Use Dijkstra's algorithm to find the cheapest path."""
 
-
-
-    def bellman_ford(self, origin, destination):
-        """Use Bellman Ford's algorithm to find the cheapest path."""
-
-        routes = []
+        routes = HeuristicHeap()
         for neighbor in self.neighbors(origin):
             price = self.get_price(origin, neighbor)
-            routes.append(Route(price=price, path=[origin, neighbor]))
+            routes.push(Route(price=price, path=[origin, neighbor]), destination=destination)
+
+        visited = set()
+        visited.add(origin)
 
         while routes:
-            old_route = routes.pop(0)
-            airport = old_route.path[-1]
 
+            # Find the nearest yet-to-visit airport
+            price, path = routes.pop()
+            airport = path[-1]
+            if airport in visited:
+                continue
+
+            # We have arrived! Wo-hoo!
+            if airport is destination:
+                return price, path
+
+            # Tentative distances to all the unvisited neighbors
             for neighbor in self.neighbors(airport):
-                new_route = Route(old_route.price + self.get_price(airport, neighbor), path=old_route.path + [neighbor])
+                if neighbor not in visited:
+                    # Total spent so far plus the price of getting there
+                    new_price = price + self.get_price(airport, neighbor)
+                    new_path = path + [neighbor]
+                    routes.push(Route(new_price, new_path), destination=destination)
 
-                if self.check_same_route(routes, new_route) and destination == new_route.path[-1]:
-                    continue
-                else:
-                    routes.append(new_route)
-
-        if routes:
-            return routes[0].price, routes[0].path
+            visited.add(airport)
 
         return float('infinity'), None
-
-    def check_same_route(self, routes: list[Route], route2: Route):
-        for i, route1 in enumerate(routes):
-            if route1.path[0] == route2.path[0] and route1.path[-1] == route2.path[-1]:
-                if route1.price > route2.price:
-                    routes[i] = route2
-                    return True
-        else:
-            return False
-
-    def bellman_ford2(self, origin, destination):
-        # Relax edges repeatedly
-        price = 0
-        for neighbor in self.neighbors(origin):
-            for u, v in self.edges():
-                weight = self.get_weight(u, v)
-                if distances[u] + weight < distances[v]:
-                    distances[v] = distances[u] + weight
-                    predecessors[v] = u
-
-        # Check for negative cycles
-        for u, v in self.edges():
-            weight = self.get_weight(u, v)
-            if distances[u] + weight < distances[v]:
-                raise ValueError("Negative cycle detected")
-
-        # Build path to destination
-        path = []
-        node = destination
-        while node is not None:
-            path.append(node)
-            node = predecessors[node]
-        path.reverse()
-
-        return distances[destination], path
-
-    def bellman_ford3(self, origin, destination):
-        """Use the Bellman-Ford algorithm to find the cheapest path."""
-
-        distances = {airport: float('inf') for airport in self._neighbors}
-        distances[origin] = 0
-
-        # Iterate over all the edges in the graph
-        for airport in self._neighbors:
-            for neighbor in self.neighbors(airport):
-                price = self.get_price(airport, neighbor)
-                # Update the distance to the neighbor
-                if distances[airport] + price < distances[neighbor]:
-                    distances[neighbor] = distances[airport] + price
-
-        # Check for negative weight cycles
-        for airport in self._neighbors:
-            for neighbor in self.neighbors(airport):
-                price = self.get_price(airport, neighbor)
-                if distances[airport] + price < distances[neighbor]:
-                    raise ValueError("Negative weight cycle detected")
-
-        # Build the path from the origin to the destination
-        path = [destination]
-        while path[-1] != origin:
-            for neighbor in self.neighbors(path[-1]):
-                if distances[neighbor] + self.get_price(path[-1], neighbor) == distances[path[-1]]:
-                    path.append(neighbor)
-                    break
-
-        # Reverse the path and return the result
-        path.reverse()
-        return distances[destination], path
--- Heap.py
+++ Heap.py
@@ -1,7 +1,11 @@
 import heapq
 
+import haversine
 
-class Heap(object):
+from models import Route, Airport
+
+
+class Heap:
     """A min-heap."""
 
     def __init__(self):
@@ -14,6 +18,33 @@ class Heap(object):
     def pop(self):
         """ Pop and return the smallest item from the heap."""
         return heapq.heappop(self._values)
+
+    def __len__(self):
+        return len(self._values)
+
+
+class HeuristicHeap:
+    """A min-heap."""
+
+    def __init__(self):
+        self._values = []
+
+    def get_distance(self, origin: Airport, destination: Airport):
+        # Haversine distance, in kilometers
+        point1 = origin.latitude, origin.longitude,
+        point2 = destination.latitude, destination.longitude
+        distance = haversine.haversine(point1, point2)
+        return distance
+
+    def push(self, value: Route, destination: Airport):
+        """Push the value item onto the heap."""
+        heuristic = self.get_distance(value.path[-1], destination)
+        priority = value.price + heuristic
+        heapq.heappush(self._values, (priority, value))
+
+    def pop(self):
+        """ Pop and return the smallest item from the heap."""
+        return heapq.heappop(self._values)[1]
 
     def __len__(self):
         return len(self._values)
